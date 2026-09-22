3521030b54ed589a435362cb3a7e0e27a0f0651e
zppliga <zppliga@users.noreply.github.com>
2026-09-22 11:50:46 +0300

    Eleventy source project for ligazpp.ru

---
+++ .eleventy.js
@@ -0,0 +1,28 @@
+const pathPrefix = process.env.PATH_PREFIX || '';
+
+module.exports = function (eleventyConfig) {
+  // Прямое копирование статики
+  eleventyConfig.addPassthroughCopy('src/style.css');
+  eleventyConfig.addPassthroughCopy('src/script.js');
+  eleventyConfig.addPassthroughCopy('src/doc');
+  eleventyConfig.addPassthroughCopy('src/CNAME');
+
+  // Базовый адрес публикации (для canonical, sitemap, Open Graph)
+  eleventyConfig.addGlobalData('siteUrl', () => process.env.SITE_URL || 'https://ligazpp.ru');
+  // Префикс пути при деплое в подпапку (например /mysite-test/)
+  eleventyConfig.addGlobalData('pathPrefix', () => pathPrefix);
+  // Запрет индексации (для тестовых сборок)
+  eleventyConfig.addGlobalData('noindex', () => process.env.NOINDEX === '1');
+
+  return {
+    dir: {
+      input: 'src',
+      output: '_site',
+      includes: '_includes',
+      data: '_data'
+    },
+    pathPrefix,
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk'
+  };
+};
